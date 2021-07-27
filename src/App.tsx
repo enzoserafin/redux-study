bf0bb9f2408a1851120aaadbfaa4ba85a609898b
enzo <enzoserafin@gmail.com>
2021-07-27 09:50:38 -0300

    Create-react-app; Clean CRA;

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-
-import store from './store';
-
-import Catalog from './components/Catalog';
-import Cart from './components/Cart';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Catalog />
-      <Cart />
-    </Provider>
+    <h1>Hello World</h1>
   );
 }
 
